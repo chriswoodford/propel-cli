34584695a98d8e719f8c75dab2f249c21e5a5e93
Chris Woodford <chris.woodford@gmail.com>
2013-05-30 14:22:58 -0400

    5 - implement composer

--- bin/propel-cli.php
+++ bin/propel-cli.php
@@ -1,16 +1,6 @@
 <?php
 
-require_once 'propel/Propel.php';
-require_once 'Doctrine/Common/ClassLoader.php';
-
-$classLoader = new \Doctrine\Common\ClassLoader('Doctrine');
-$classLoader->register();
-
-$classLoader = new \Doctrine\Common\ClassLoader('Symfony', 'Doctrine');
-$classLoader->register();
-
-$classLoader = new \Doctrine\Common\ClassLoader('PropelCli');
-$classLoader->register();
+require_once 'vendor/autoload.php';
 
 $helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
 
